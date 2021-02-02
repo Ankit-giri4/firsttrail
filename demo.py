da993467bfa583270a6d9c3262336a36029ad8a5
ANKIT <giriankit046@gmail.com>
2021-02-02 22:02:25 +0530

    "all commited"

--- demo.py
+++ demo.py
@@ -2,4 +2,4 @@ print("Hello_world!")
 
 print("I have just created new branch dev")
 print("This is de v branch and we're now puling it on remote repo")
-print("New changed in dev branch")
+print("New changefffd in dev branch")
--- forloop.py
+++ forloop.py
@@ -1,5 +1,5 @@
 for i in range(0, 4):
     print("yuppppp")
 
-print("Done with for loop")
+print("Done with ffffffor loop")
 print("Done with  new for loop")
